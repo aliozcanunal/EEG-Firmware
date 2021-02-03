1020369bde789c1835e09be725631ff3640d0754
aliozcanunal <aozcanunal@gmail.com>
2021-02-03 03:23:57 +0300

    .gitignore

--- EEG-Firmware/Core/Src/main.c
+++ EEG-Firmware/Core/Src/main.c
@@ -4,16 +4,11 @@
   * @file           : main.c
   * @brief          : Main program body
   ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
+Created by Ali Özcan
+
+
+
+
   ******************************************************************************
   */
 /* USER CODE END Header */
@@ -275,4 +270,4 @@ void assert_failed(uint8_t *file, uint32_t line)
 }
 #endif /* USE_FULL_ASSERT */
 
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+
